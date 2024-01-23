f4658b73215b67da8e1b7f7319f0fbac6da23aea
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-01-23 16:22:15 +0000

    as

--- Streamlit.py
+++ Streamlit.py
@@ -9,12 +9,12 @@ import seaborn as sns
 import streamlit as st
 
 
-
+# Usamos el decorador de caché para almacenar los resultados de esta función
+@st.cache
 def importar_datos(posicion):
-    filename = f"/workspaces/Proyecto_Knn_Players/Data Posición/df_{posicion}_medias.csv"
-    df = pd.read_csv(filename, encoding='ISO-8859-1')
+    filename = f"Data Posición/df_{posicion}_medias.csv"
+    df = pd.read_csv(filename)
     return df
-
 
 
 st.write("Bienvenido a la página web de Fútbol Estadísticas!")
@@ -23,7 +23,7 @@ opciones = ["delanteros", "medio", "defensas", "porteros"]
 opcion = st.selectbox("¿Qué posición deseas consultar?", opciones)
 
 dataframe = importar_datos(opcion)
-
+# Aquí puedes agregar código para mostrar el DataFrame en tu aplicación Streamlit
 
 
 
