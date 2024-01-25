b73cac49994a3fa03a0e98b6bfece04a15561943
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2024-01-25 21:29:29 +0000

    a

--- Funciones.py
+++ Funciones.py
@@ -94,7 +94,7 @@ def buscar_jugadores_similares_defensas(nombre):
         st.plotly_chart(fig1)
         
        # Configura una cuadrícula de subplots con 3 filas y 2 columnas
-        fig, axs = plt.subplots(3, 2, figsize=(20, 12))  # Ajusta el tamaño según sea necesario
+        fig, axs = plt.subplots(3, 2, figsize=(20, 10))  # Ajusta el tamaño según sea necesario
         # Gráfico de dispersión para Clearances
         for i, name in enumerate(jugadores_similares['Name']):
             axs[0, 0].scatter(i, jugadores_similares.loc[jugadores_similares['Name'] == name, 'Clearances'], color=colores[i % len(colores)], marker='o')
